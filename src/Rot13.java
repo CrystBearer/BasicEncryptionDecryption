80bc170943fe5fe7f17743c3487a3ba191972ca4
Vin <54550661+CrystBearer@users.noreply.github.com>
2019-12-25 21:54:48 -0800

    Tests (#1)
    * Modified rot13 algorithm
    
    * Added comments and removed unused imports
    
    Signed-off-by: Vivian Hoang <vivianhoang@protonmail.ch>
    
    * Changed algorithm, added tests.
    
    Signed-off-by: Vivian Hoang <vivianhoang@protonmail.ch>

--- src/Rot13.java
+++ src/Rot13.java
@@ -1,19 +1,27 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
+/**
+ * Algorithm that shifts the character by the key
+ */
 public class Rot13 implements Algorithm {
+    /**
+     * Encrypts the given string input
+     * @param input Plaintext to encrypt
+     * @param key used to decrypt by that value
+     * @return String of the decrypted string
+     */
     public String encrypt(String input, String key){
         String output = "";
         int shift =  Integer.parseInt(key);
         for(char s: input.toCharArray()){
-            if(!Character.isAlphabetic(s)) {
+            if(!Character.isAlphabetic(s)){
                 output += s;
                 continue;
             } else {
                 int sum = s + shift;
                 char o =(char) (sum);
-                if(sum > (int) 'z' && !Character.isUpperCase(s) || sum > (int) 'Z' && Character.isUpperCase(s)) {
-                    o = (char)(o - 26);
+                if(Character.isAlphabetic(s)) {
+                    if (sum > (int) 'z' && !Character.isUpperCase(s) || sum > (int) 'Z' && Character.isUpperCase(s)) {
+                        o = (char) (o - 26);
+                    }
                 }
                 output += o;
             }
@@ -21,18 +29,26 @@ public class Rot13 implements Algorithm {
         return output;
     }
 
+    /**
+     * Decrypts the given string input
+     * @param input Ciphertext to decrypt
+     * @param key used to decrypt by that value
+     * @return String of the decrypted string
+     */
     public String decrypt(String input, String key){
         String output = "";
         int shift =  Integer.parseInt(key);
         for(char s: input.toCharArray()){
-            if(!Character.isAlphabetic(s)) {
+            if(!Character.isAlphabetic(s)){
                 output += s;
                 continue;
             } else {
                 int sum = s - shift;
                 char o =(char) (sum);
-                if(sum < (int) 'a' && !Character.isUpperCase(s) || sum < (int) 'A' && Character.isUpperCase(s)) {
-                    o = (char)(o + 26);
+                if(Character.isAlphabetic(s)){
+                    if(sum < (int) 'a' && !Character.isUpperCase(s) || sum < (int) 'A' && Character.isUpperCase(s)) {
+                        o = (char)(o + 26);
+                    }
                 }
                 output += o;
             }
